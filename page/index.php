8de8343dd46dad44a0c700bb60b67284b15036f1
Stanislav E <evendot@yandex.ru>
2022-10-02 01:53:35 +0300

    Started making a design, added Bootstrap, changed docker's port

--- page/index.php
+++ page/index.php
@@ -1,9 +1,17 @@
-< !DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
 <html>
-    <header>
-        <tytle>FriendKraft</tytle>
-    </header>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <tytle>FriendKraft</tytle>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
     <body>
-
+    <header>Шапка</header>
+    <aside>Левая колонка</aside>
+    <main>Центр</main>
     </body>
 </html>
