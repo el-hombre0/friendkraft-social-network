d04b0a7c72a5026f22d1cd553e5108a69778a854
Stanislav E <evendot@yandex.ru>
2022-12-04 11:27:03 +0300

    Fixed html user_menu.php

--- public_html/html/user_menu.php
+++ public_html/html/user_menu.php
@@ -1,11 +1,12 @@
 <?
+$db_connect = pg_connect("host=localhost dbname=postgres port=5432 user=postgres password=password");
 if(!$_SESSION['email'] AND !$_SESSION['password']){
 }else{
     $id=$_GET['id'];
-    $informer=mysql_query("SELECT count(id) FROM message WHERE poluchatel='{$_SESSION['id']}' AND ready='0'");
-    $row=mysql_fetch_array($informer, MYSQL_NUM);
-     $informer_2=mysql_query("SELECT count(id) FROM friends WHERE id_user_2='{$_SESSION['id']}' AND status='1'");
-    $row_2=mysql_fetch_array($informer_2, MYSQL_NUM);
+    $informer=pg_query($db_connect, "SELECT count(id) FROM message WHERE poluchatel='{$_SESSION['id']}' AND ready='0'");
+    $row=pg_fetch_array($informer, PGSQL_NUM);
+     $informer_2=pg_query($db_connect, "SELECT count(id) FROM friends WHERE id_user_2='{$_SESSION['id']}' AND status='1'");
+    $row_2=pg_fetch_array($informer_2, PGSQL_NUM);
     echo"<div class=menu_user>
     <a href=index?id=".$_SESSION['id'].">Моя страница</a><br>
         <a href=novosti>Новости</a><br>
